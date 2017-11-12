c4fca04cad64841240d9ab4f29edbe0aa4c86514
Saroj Pradhan <pradhansk90@gmail.com>
2017-11-12 10:48:03 +0530

    first commit

---
+++ src/main/java/com/example/demo/Poc1SpringBootSdjpaApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Poc1SpringBootSdjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Poc1SpringBootSdjpaApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+
+	@RequestMapping(value = "/greeting/{name}", method = RequestMethod.GET)
+	public String sayHello(@PathVariable String name) {
+		return "hello  "+name;
+
+	}
+
+}
