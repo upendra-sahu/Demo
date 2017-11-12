8610ed8bf7232a0bcf7258b564a2a4629d80c8c1
Saroj Pradhan <pradhansk90@gmail.com>
2017-11-12 19:38:08 +0530

    added changes related to war

--- src/main/java/com/example/demo/Poc1SpringBootSdjpaApplication.java
+++ src/main/java/com/example/demo/Poc1SpringBootSdjpaApplication.java
@@ -2,10 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Poc1SpringBootSdjpaApplication {
+public class Poc1SpringBootSdjpaApplication extends SpringBootServletInitializer{
 
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		// TODO Auto-generated method stub
+		return application.sources(Poc1SpringBootSdjpaApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(Poc1SpringBootSdjpaApplication.class, args);
 	}
